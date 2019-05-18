8fec258c37cf38c4af2ffe64d67e4f3fc121ad3b
Hao Chen <inbox@haochen.name>
2019-05-17 19:10:45 -0500

    get gene names

--- extract_gene_symb_name_from_ncbi_gene_info.sh
+++ extract_gene_symb_name_from_ncbi_gene_info.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #-e "s/\(|\)/ /g"  -e "s/\[|\]/ /g" 
-grep ^9606 ~/Downloads/gene_info |grep -v ^LOC|grep -v -i pseudogene |cut -f 3,5,12|sed -e "s/\t-//"  -e "s/\t/|/2"  -e "s/\t-//"  -e "s/\t/\|/" -e "s/(\|)\|\[\|\]\|{\|}/ /g" | sort >ncbi_gene_symb_syno_name_txid9606.txt 
+grep ^9606 ~/Downloads/gene_info |cut -f 3,5,12|grep -v ^LOC|grep -v -i pseudogene |sed -e "s/\t-//"  -e "s/\t/|/2"  -e "s/\t-//"  -e "s/\t/\|/" -e "s/(\|)\|\[\|\]\|{\|}/ /g" | sort >ncbi_gene_symb_syno_name_txid9606.txt 
 
