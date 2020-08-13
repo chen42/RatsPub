3e2b68aea99edd81f8f10843371e8ac619ba6c92
Hao Chen <inbox@haochen.name>
2020-08-13 04:57:04 -0500

    update gwas cat

---
+++ ebi_to_ratspub.sh
@@ -0,0 +1,3 @@
+# ./gwas_catalog_v1.0.2-associations_e100_r2020-08-05.tsv
+cut -f 2,8,14,15,28,35,22  $1 |grep -P -i "schizo|autism|smoking|addiction|cocaine|opioid|morphin|cannabis|depression|anxiety|bipolar|behavior|mental|amphetamine|nicotine|chornic obstructive|psychiatric|methadone|heroin|alcohol|drug dependence"|grep -v -i intergenic |grep -v BMI >addiction_gwas.tsv
+
